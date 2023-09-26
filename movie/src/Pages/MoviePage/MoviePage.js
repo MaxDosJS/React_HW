6debdeb0831f27d296db474cdbbc6a2b09b23256
MaxDosJS <122891450+MaxDosJS@users.noreply.github.com>
2023-09-26 14:50:49 +0600

    Add files via upload

---
+++ movie/src/Pages/MoviePage/MoviePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./MoviePage.css"
+
+const MoviePage = () => {
+
+  return (
+   <div className="movie_poster_block"> String</div>
+  )
+}
+
+export default MoviePage
